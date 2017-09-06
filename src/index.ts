e72d1bc6091eb11875e75c9594be5e8faf57d8e4
HuJian <624598206@qq.com>
2017-09-06 21:56:32 +0800

    feat(index): update

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
 import * as Rx from 'rxjs';
 
 window.onload = () => {
+    // 获取clock
     const elClock = document.getElementById("clock");
+    // 获取当前时间
     const getTime =  function(){
         const _ = ['00','01','02','03','04','05','06','07','08','09'],  //补零
             d = new Date(), h = d.getHours(),m = d.getMinutes(),s = d.getSeconds();
     	return [_[h]||h,_[m]||m,_[s]||s].join(":");
     };
-    let trickSteam = Rx.Observable.create(observer => {
+    // 创建一个名为trickSteam的Observable
+    const trickSteam = Rx.Observable.create(observer => {
         setInterval(() => {
             observer.next(getTime());
         }, 1000);
     })
 
-    let uiRefresher = {
+    // 这是trickSteam的Observer
+    const uiRefresher = {
         next: (data: string) => {
             elClock.textContent = data
         },
         error: err => console.log(err),
         completed: () => console.log('Observer got a complete notification')
     }
+    // 订阅
     trickSteam.subscribe(uiRefresher);
+
+
+    // 从事件中创建Observable（Pure，no side effect）
+    const button = document.querySelector('button');
+    Rx.Observable.fromEvent(button, 'click')
+    .throttleTime(1000)
+    .mapTo(1)
+    .scan(count => count + 1, 0)
+    .subscribe(count => console.log(`Clicked ${count} times`));
 }
